590f6d1998b9eb2571926cb9009033d35f8ef7d4
Matthew Hagan <matthewhagan15@gmail.com>
2025-07-19 17:40:24 -0400

    changes

--- client_conn.js
+++
@@ -1,9 +0,0 @@
-
-console.log("hello from js");
-const viewport = document.querySelector("h1");
-viewport.textContent = "Now we really in here.";
-var requestBox = document.getElementById("request_box");
-var requestBoxButton = document.getElementById("request_box_button");
-requestBoxButton.addEventListener('click', () => {
-    console.log("click on introPar");
-})
